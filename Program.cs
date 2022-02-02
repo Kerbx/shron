e3a47362e27e9a6292e2126051af9ca131f54de3
Lukich <55893122+Kerbx@users.noreply.github.com>
2022-02-02 15:39:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace kurwa
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            string birthDate = Console.ReadLine();
+            string adress = Console.ReadLine();
+            string country = Console.ReadLine();
+            char gender = (char)Console.Read();
+            
+            string kurs = "Разработка программных модулей.";
+            double result = 100.00f;
+            int zanyatiya = 3;
+            string prepod = "Жукова М.А.";
+            
+            Console.WriteLine($"Информация о студенте:\nИмя: {name};\nФамилия: {surname};\nДата рождения: {birthDate};\nАдрес: {adress};\nПол: {gender};\nСтрана: {country};\nИнформация о курсе:\nНазвание курса: {kurs};\nБаллы: {result};\nКоличество занятий в неделю: {zanyatiya};\nПреподаватель: {prepod}\n");
+            Console.ReadKey();
+            */
+
+            /*
+            int firstNumber = int.Parse(Console.ReadLine());
+            double secondNumber = int.Parse(Console.ReadLine());
+            double avg = (firstNumber + secondNumber) / 2;
+            Console.WriteLine(avg);
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine("Введите сторону треугольника:\n");
+            double strona = int.Parse(Console.ReadLine());
+            double sqrt = Math.Sqrt(3);
+
+            Console.WriteLine($"Периметр: {strona * 3}");
+            Console.WriteLine($"Площадь: {(strona * strona * sqrt) / 4  }");
+            Console.ReadKey();
+            */
+
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a < b && b < c);
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine("Введите четыре числа:\n");
+            int x1= int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            int x4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x1 + x2 > x3 + x4);
+            Console.ReadKey();
+            */
+
+            /*
+            Console.WriteLine("Введите число:\n");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num >= 0)
+            {
+                Console.WriteLine("Введенное число положительное.");
+            }
+            else
+            {
+                Console.WriteLine("Введенное число отрицательное.");
+            }
+
+            Console.ReadKey();
+            */
+
+            Console.WriteLine("Введите двузначное число.");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Десятки: {Math.Abs(num) / 10}; единицы: {Math.Abs(num) % 10};");
+            Console.ReadKey();
+
+        }
+    }
+}
