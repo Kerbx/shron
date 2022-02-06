c16d89c921d4a0b346599ee82febca929328aa0b
Lukich <55893122+Kerbx@users.noreply.github.com>
2022-02-06 12:01:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace kurwa
+namespace kurwaconsole
 {
     class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
+
             // Первый файл.
             // Задание 1.
             /*
@@ -20,12 +21,12 @@ namespace kurwa
             string adress = Console.ReadLine();
             string country = Console.ReadLine();
             char gender = (char)Console.Read();
-            
+
             string kurs = "Разработка программных модулей.";
             double result = 100.00f;
             int zanyatiya = 3;
             string prepod = "Жукова М.А.";
-            
+
             Console.WriteLine($"Информация о студенте:\nИмя: {name};\nФамилия: {surname};\nДата рождения: {birthDate};\nАдрес: {adress};\nПол: {gender};\nСтрана: {country};\nИнформация о курсе:\nНазвание курса: {kurs};\nБаллы: {result};\nКоличество занятий в неделю: {zanyatiya};\nПреподаватель: {prepod}\n");
             Console.ReadKey();
             */
@@ -40,6 +41,7 @@ namespace kurwa
             */
 
             // Задание 3
+            /*
             Console.WriteLine("Введите сторону треугольника:\n");
             double strona = int.Parse(Console.ReadLine());
             double sqrt = Math.Sqrt(3);
@@ -47,6 +49,7 @@ namespace kurwa
             Console.WriteLine($"Периметр: {strona * 3}");
             Console.WriteLine($"Площадь: {(Math.Pow(strona, 2.0f) * sqrt) / 4  }");
             Console.ReadKey();
+            */
 
             // Задание 4
             /*
@@ -206,6 +209,191 @@ namespace kurwa
             Console.WriteLine($"Результат: {size}-й день недели - {result}");
             */
 
+            // Файл 3
+            // Задание 1
+            /*
+
+            for(int i = -3; i <= 24; i += 3)
+            {
+                Console.WriteLine($"{i} ");
+            }
+            */
+
+            // Задание 2
+            /*
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            for(int i = 99; i >=1; i--)
+            {
+                Console.Write($"{i} ");
+            }
+            */
+
+            // Задание 3
+            /*
+            int negative = 0;
+            int positive = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                if(temp > 0)
+                {
+                    positive++;
+                }
+                else if(temp < 0)
+                {
+                    negative++;
+                }
+            }
+
+            Console.WriteLine($"Количество положительных чисел: {positive}\nКоличество отрицательных чисел: {negative}");
+            */
+
+            // Задание 4
+            /*
+            int sum = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Сумма введенных чисел равна: {sum}");
+            */
+
+            // Задание 5
+            /*
+            int sum = 0;
+
+            for(int i = 1; i < 19; i += 2)
+            {
+                sum += i;
+                Console.Write($"{i} + ");
+            }
+
+            Console.Write($"=> Сумма = {sum}");
+            */
+
+            // Задание 6
+            /*
+            double power = 0.0f;
+
+            for(int i = 0; i < 10; i++)
+            {
+                power *= double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Произведение чисел равно: {power}");
+            */
+
+            // Задание 7
+            /*
+            for(int i = 2; i < 8; i++)
+            {
+                for(double pow = 2.0f; i < 5; i++)
+                {
+                    Console.WriteLine($"z(x,y) = {i} ^ {pow} = {Math.Pow(i, pow)}");
+                }
+            }
+            */
+
+            // Задание 8
+            /*
+            for(int i = 30; i < 30; i++)
+            {
+                for(int y = 1; y < 5; y++)
+                {
+                    Console.WriteLine($"z(x,y) = {i} - {y} = {i - y}");
+                }
+            }
+            */
+
+            // Файл 4
+            // Задание 1
+            /*
+            int counter1 = 3;
+            while (counter1 <= 21)
+            {
+                Console.Write($"{counter1} ");
+                counter1 += 2;
+            }
+
+            int counter2 = 3;
+            do
+            {
+                Console.Write($"{counter2} ");
+                counter2 += 2;
+            } while (counter2 <= 21);
+            */
+
+            // Задание 2
+            /*
+            int counter1 = 15;
+            while (counter1 <= 0)
+            {
+                Console.Write($"{counter1} ");
+                counter1 -= 3;
+            }
+
+            int counter2 = 15;
+            do
+            {
+                Console.Write($"{counter2} ");
+                counter2 -= 3;
+            } while (counter2 <= 0);
+            */
+
+            // Задание 3
+            /*
+            int product = 10;
+            int power = 1;
+
+            while(product <= 20)
+            {
+                power *= product;
+                Console.Write($"{product} * ");
+                product += 2;
+            }
+            Console.Write($"= {power}");
+            */
+
+            // Задание 4
+            /*
+            double sum = 0;
+            int counter = 1;
+
+            while(counter <= 5)
+            {
+                double num = double.Parse(Console.ReadLine());
+                sum += num;
+                counter++;
+            }
+
+            Console.WriteLine($"Сумма введенных чисел равна: {sum}");
+            */
+
+            // Задание 5
+            /*
+            int counter = 1;
+            int maximum = 0;
+            int prev = 0;
+
+            do
+            {
+                maximum = int.Parse(Console.ReadLine());
+                if (maximum > prev)
+                {
+                    prev = maximum;
+                    counter++;  
+                }
+            } while (maximum != 0);
+
+            Console.WriteLine($"Максимальное число {prev}, его порядковый номер {counter}");
+            */
         }
     }
 }
+
